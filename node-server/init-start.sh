e44fc6422136911fe275b2e2fa7d121589df826c
zixiao-macos <zixiao-macos@yeah.net>
2022-11-07 09:52:16 +0800

    add

--- node-server/init-start.sh
+++ node-server/init-start.sh
@@ -8,7 +8,7 @@ sed -i "$ a source /etc/environment" ~/.bashrc
 #定时任务
 echo "* * * * * date >> /opt/build/test.md 2>&1" >> ~/init-crontab
 echo "* * * * * /opt/one_consumer.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
-echo "* * * * * /opt/three_publish.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
+#echo "* * * * * /opt/three_publish.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
 echo "0 0 * * * /opt/RestartApi.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
 echo "*/5 * * * * /opt/RestartCrontab.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
 crontab ~/init-crontab
