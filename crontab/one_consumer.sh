d3526bc46600652b75cab9518ca44254824dce68
zixiao-macos <zixiao-macos@yeah.net>
2022-11-15 15:55:39 +0800

    add

--- crontab/one_consumer.sh
+++ crontab/one_consumer.sh
@@ -5,8 +5,12 @@ for (( i = 0; i < 60; i=(i+step) )); do
     #获取交易对最新区块高度
     curl http://127.0.0.1:9090/consumer/set_height
     curl http://127.0.0.1:9090/consumer/set_height
+    curl http://127.0.0.1:9090/consumer/set_height
 
     #消费获取交易对事件
+    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
@@ -15,8 +19,14 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
 
     #设置SWAP交易
+    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
@@ -25,12 +35,18 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
 
     #设置移除流动池
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
-} &
+    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
+}
     sleep $step
 
 done
