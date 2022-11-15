19e54b3f501436e4c41902ab35e7e495a9f9aa9c
zixiao-macos <zixiao-macos@yeah.net>
2022-11-15 15:52:34 +0800

    add

--- crontab/one_consumer.sh
+++ crontab/one_consumer.sh
@@ -5,14 +5,8 @@ for (( i = 0; i < 60; i=(i+step) )); do
     #获取交易对最新区块高度
     curl http://127.0.0.1:9090/consumer/set_height
     curl http://127.0.0.1:9090/consumer/set_height
-    curl http://127.0.0.1:9090/consumer/set_height
-    curl http://127.0.0.1:9090/consumer/set_height
 
     #消费获取交易对事件
-    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
-    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
-    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
-    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
@@ -21,16 +15,8 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
-    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
-    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
-    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
-    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
 
     #设置SWAP交易
-    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
@@ -39,16 +25,8 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
 
     #设置移除流动池
-    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
-    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
