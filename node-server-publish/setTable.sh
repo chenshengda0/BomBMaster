462f93fb6fccaaca23c69740360b463511e2c2d3
zixiao-macos <zixiao-macos@yeah.net>
2022-11-17 12:02:26 +0800

    add

--- node-server-publish/setTable.sh
+++ node-server-publish/setTable.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 for i in {0..999}
 do
-    echo "ALTER TABLE sp_liquidity_transaction_${i} CHANGE token0_count token0_count DECIMAL(50,18) NOT NULL COMMENT 'token0数量';"
-    echo "ALTER TABLE sp_liquidity_transaction_${i} CHANGE token1_count token1_count DECIMAL(50,18) NOT NULL COMMENT 'token1数量';"
+    #echo "ALTER TABLE sp_liquidity_transaction_${i} CHANGE token0_count token0_count DECIMAL(65,18) NOT NULL COMMENT 'token0数量';"
+    #echo "ALTER TABLE sp_liquidity_transaction_${i} CHANGE token1_count token1_count DECIMAL(65,18) NOT NULL COMMENT 'token1数量';"
+    echo "ALTER TABLE sp_transaction_${i} CHANGE from_number from_number DECIMAL(65,18) NOT NULL COMMENT '输入数量';"
+    echo "ALTER TABLE sp_transaction_${i} CHANGE to_number to_number DECIMAL(65,18) NOT NULL COMMENT '输出数量';"
 done
