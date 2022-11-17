efeddb29bce8e0a65af0deb2f583b44884e3c148
zixiao-macos <zixiao-macos@yeah.net>
2022-11-17 11:04:33 +0800

    add

--- node-server-api/init-start.sh
+++ node-server-api/init-start.sh
@@ -6,10 +6,10 @@ echo -e "Y\n" | unminimize
 for item in $(cat /proc/1/environ | tr '\0' '\n');do echo "export ${item}" >>  /etc/environment;done
 sed -i "$ a source /etc/environment" ~/.bashrc
 #定时任务
-echo "* * * * * date >> /opt/build/test.md 2>&1" >> ~/init-crontab
+echo "* * * * * date >> /opt/build/node-server-api.md 2>&1" >> ~/init-crontab
 #echo "* * * * * /opt/one_consumer.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
 #echo "* * * * * /opt/three_publish.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
-echo "0 0 * * * /opt/RestartApi.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
+echo "0 0 * * * /opt/RestartApi.sh >> /opt/build/node-server-api.md 2>&1" >> ~/init-crontab
 #echo "* * * * * /opt/RestartCrontab.sh >> /opt/build/test.md 2>&1" >> ~/init-crontab
 crontab ~/init-crontab
 rm -rf ~/init-crontab
