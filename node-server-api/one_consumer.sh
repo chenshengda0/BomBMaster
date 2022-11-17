ecf1d8263af29e705460241e21a409a59c6b3908
zixiao-macos <zixiao-macos@yeah.net>
2022-11-17 11:10:13 +0800

    add

--- node-server-api/one_consumer.sh
+++ node-server-api/one_consumer.sh
@@ -11,14 +11,8 @@ for (( i = 0; i < 60; i=(i+step) )); do
     #消费获取V2交易对事件
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
 
-    #设置SWAP交易
-    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
-
-    #设置添加流动池
-    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
-
-    #设置移除流动池
-    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
+    #设置交易事件
+    curl http://127.0.0.1:9090/consumer/setTransEvents
 } &
     sleep $step
 
