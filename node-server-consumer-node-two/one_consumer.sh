d39d3d4b20e5ce5eae2ad2f79dfa1544be4e5620
zixiao-macos <zixiao-macos@yeah.net>
2022-11-18 17:48:27 +0800

    add

--- node-server-consumer-node-two/one_consumer.sh
+++ node-server-consumer-node-two/one_consumer.sh
@@ -7,10 +7,7 @@ for (( i = 0; i < 60; i=(i+step) )); do
 
     #消费获取交易对事件
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
-
-    #消费获取V2交易对事件
-    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
-
+    
     #设置交易事件
     curl http://127.0.0.1:9090/consumer/setTransEvents
 } &
